780de68d83b2a263d571a441fa5bcb172562ec29
devmalya99 <debmalyamazumdar99@gmail.com>
2024-05-09 23:59:26 +0530

    toggling tabs v6.3

--- src/components/TabsComponent.jsx
+++ src/components/TabsComponent.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
+
+const list = [
+  {
+    "title": 'Grid View',
+    "id": 'A1'
+  }
+  ,
+  {
+    "title": 'List View',
+    "id": 'A2'
+  }
+
+]
 const TabsComponent = () => {
+  const [activeID,setActiveId] = useState('')
+  const handleClick=(id)=>{
+     
+  }
   return (
     <div className="border-2 rounded-xl w-full lg:w-[700px] py-2 mt-8 mb-8">
     <div role="tablist" className="tabs tabs-boxed">
-<a role="tab" className="tab text-xl">Grid View</a>
-<a role="tab" className="tab tab-active text-xl">List View</a>
+      {
+        list.map((each)=>{
+          return (
+            <Link to key={each.id} role="tab" 
+            className={`tab text-xl ${each.id===activeID ? 'tab-active':''}` }
+            onClick={()=>setActiveId(each.id)}
+            >{each.title}</Link>
+          )
+        })
+      }
+
 </div>
     </div>
   )
