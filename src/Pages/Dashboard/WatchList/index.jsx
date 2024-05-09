dffb239eaa259b2ea2119e21914a973ab674de9b
devmalya99 <debmalyamazumdar99@gmail.com>
2024-05-09 21:39:42 +0530

    empty watchlist handled

--- src/Pages/Dashboard/WatchList/index.jsx
+++ src/Pages/Dashboard/WatchList/index.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import CryptoPriceCard from '../../../components/Cards/CryptoPriceCard'
+import {motion} from 'framer-motion'
+import { useNavigate } from 'react-router-dom'
 
 const WatchList = () => {
   const arr = useSelector((store)=>store.watchList.list)
   console.log(arr)
+  const navigate = useNavigate()
+
+  const handleHome =()=>{
+    navigate ('/home')
+  }
   return (
-    <div className=''>
+    <motion.div className=''
+    initial={{ opacity: 0, x: -50 }}
+    animate={{ opacity: 1, x: 0 }}
+    transition={{ delay: 0.5, duration: 1 }}>
+      {
+        arr.length===0 && 
+        <div className='text-center flex flex-col items-center'>
+
+          <p className='text-5xl mt-16'>Its lonely here....</p>
+
+        <p className=' mb-4 text-5xl mt-8'>How about we add something</p>
+
+        <button className='bg-blue-700 px-6 py-2 rounded-xl font-bold text-2xl mt-8'
+        onClick={()=>handleHome()}>Go to Home</button>
+        
+        </div>
+      }
       {
         arr.map((each)=>{
           return(
@@ -17,7 +40,7 @@ const WatchList = () => {
         })
       }
       
-    </div>
+    </motion.div>
   )
 }
 
